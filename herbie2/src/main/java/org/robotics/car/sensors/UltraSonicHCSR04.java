65722a1b739ebc24e33694f500dfc1b84f291852
rogerrut <roger.ruttimann@godorsal.com>
2016-12-10 14:59:06 -0800

    Updated license across project. Using Apache 2.0

--- herbie2/src/main/java/org/robotics/car/sensors/UltraSonicHCSR04.java
+++ herbie2/src/main/java/org/robotics/car/sensors/UltraSonicHCSR04.java
@@ -1,29 +1,30 @@
 package org.robotics.car.sensors;
 
-/**
-* Implementing Sonic Sensor Model HC-SR04
-* Datasheet: http://www.micropik.com/PDF/HCSR04.pdf
-*
-* @author Roger Ruttimann
-* 
-***********************************************
-* Copyright 2016 Roger Ruttimann
-* 
-* 
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-* 
-*     http://www.apache.org/licenses/LICENSE-2.0
-* 
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License. 
-*
-**/
 
+/*
+Copyright 2016 Autonomous Open Source Project
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Author: Roger Ruttimann
+ */
+
+/**
+ * Implementing Sonic Sensor Model HC-SR04
+ * Datasheet: http://www.micropik.com/PDF/HCSR04.pdf
+ *
+ * @author Roger Ruttimann
+ */
 
 import org.robotics.car.sensors.Sensor;
 import org.robotics.car.exception.TimeoutException;
