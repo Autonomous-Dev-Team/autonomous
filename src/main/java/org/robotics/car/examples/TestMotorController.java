defd9d77cdafa2ce8c9c7a7b0c8656a5ef1d262a
tresgy <maurice.j.ruttimann@gmail.com>
2017-03-18 17:08:53 -0700

    Cleanup

--- src/main/java/org/robotics/car/examples/TestMotorController.java
+++ src/main/java/org/robotics/car/examples/TestMotorController.java
@@ -48,6 +48,9 @@ public class TestMotorController {
         System.out.println("Turn right for 180 degrees ..");
         motorController.right(180);
 
+        motorController.stop();
+
+
     }
 
 }
