6fc54e1998154c4269ea2ef6c5f3cb0b1f64b5cd
tresgy <maurice.j.ruttimann@gmail.com>
2017-04-18 21:26:02 -0700

    Ben implemented all the logistics of the sensor

--- src/main/java/org/robotics/car/controller/CarEngine.java
+++ src/main/java/org/robotics/car/controller/CarEngine.java
@@ -20,6 +20,7 @@ package org.robotics.car.controller;
 
 
 // Imports for the RaspberryPI libraries that give access to the hardware
+
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
@@ -33,19 +34,18 @@ import org.robotics.car.sensors.UltraSonicHCSR04;
 
 /**
  * This code was based on the sample code from Robert Savage member of the Pi4j team
- * 
+ * <p>
  * Enhanced for the use in the Innovation Lab at TL for educational purposes.
- * 
+ *
  * @author Roger Ruttimann
- * 
  */
 public class CarEngine {
-	
-	// Constants
-	static final int DEFAULT_LOOP_TIME = 300; // 1 sec
+
+    // Constants
+    static final int DEFAULT_LOOP_TIME = 300; // 1 sec
 
     // Minimal distance between sensor and object to do an action
-    static float MINIMAL_DISTANCE = 7;
+    static float MINIMAL_DISTANCE = 4;
     static int SLEEPING_TIME_BETWEEN_LOOP = 500; // in ms
     static int SAMPLESIZE = 3; //samplesize for average calculation
     static int DEGREES_TO_TURN = 90;
@@ -57,9 +57,9 @@ public class CarEngine {
     static MotorController motorController = null;
 
     // LED instances
-    static final GpioPinDigitalOutput ledGreen	= gpio.provisionDigitalOutputPin(RaspiPin.GPIO_27, "CarEngine", PinState.LOW);
-    static final GpioPinDigitalOutput ledBlue	= gpio.provisionDigitalOutputPin(RaspiPin.GPIO_28, "CarForward", PinState.LOW);
-    static final GpioPinDigitalOutput ledYellow	= gpio.provisionDigitalOutputPin(RaspiPin.GPIO_29, "CarReverse", PinState.LOW);
+    static final GpioPinDigitalOutput ledGreen = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_27, "CarEngine", PinState.LOW);
+    static final GpioPinDigitalOutput ledBlue = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_28, "CarForward", PinState.LOW);
+    static final GpioPinDigitalOutput ledYellow = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_29, "CarReverse", PinState.LOW);
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////
     // Methods
@@ -77,7 +77,7 @@ public class CarEngine {
 
         // Initialize Sensors, motor controller
         // Create an instance of the Sonic Sensor using GPIO Pin #4 for ECHO signal and GPIO Pin # 5 for TRIGGER
-        UltraSonicHCSR04 rightsensor = new UltraSonicHCSR04("Right sensor", 16, 6);
+        UltraSonicHCSR04 rightSensor = new UltraSonicHCSR04("Right sensor", 16, 6);
         UltraSonicHCSR04 leftSensor = new UltraSonicHCSR04("Left sensor", 4, 17);
         UltraSonicHCSR04 frontright = new UltraSonicHCSR04("Front Right sensor", 24, 25);
         UltraSonicHCSR04 frontmiddle = new UltraSonicHCSR04("Front Middle sensor", 22, 23);
@@ -85,7 +85,7 @@ public class CarEngine {
 
         // Start measurement for each sensor
         try {
-            rightsensor.start();
+            rightSensor.start();
             Thread.sleep(163);
             leftSensor.start();
             Thread.sleep(157);
@@ -108,5 +108,258 @@ public class CarEngine {
         System.out.println("Sensors initialized and ready to measure distances...");
 
 
+        // Create Motor controller
+        motorController = new MotorController(terrain, "M1", "M2", "M3", "M4");
+
+        // local variables
+        float frontRightMeasuredDistance = 0;
+        float frontLeftMeasuredDistance = 0;
+        float frontMiddleMeasuredDistance = 0;
+        float leftMeasuredDistance = 0;
+        float rightMeasuredDistance = 0;
+
+        System.out.println("Starting car Engine vrooom...");
+
+        // Car ENgine started turn on Green LED
+        ledGreen.high();
+
+        // Car is not moving turn off other LED's
+        ledBlue.low();
+        ledYellow.low();
+
+
+        System.out.println("Autonomous is Online and Ready to Go");
+        try {
+            // Run forever
+            while (true) {
+                // Read all the measurements
+                frontRightMeasuredDistance = frontright.getDistance();
+                frontLeftMeasuredDistance = frontleft.getDistance();
+                frontMiddleMeasuredDistance = frontmiddle.getDistance();
+                leftMeasuredDistance = leftSensor.getDistance();
+                rightMeasuredDistance = rightSensor.getDistance();
+
+                System.out.println("Autonomous : Read distance now decides what to do ...");
+                //Logic table
+
+                if ((frontLeftMeasuredDistance > MINIMAL_DISTANCE)
+                        && (frontMiddleMeasuredDistance > MINIMAL_DISTANCE)
+                        && (frontRightMeasuredDistance > MINIMAL_DISTANCE)
+                        && (leftMeasuredDistance > MINIMAL_DISTANCE)
+                        && (rightMeasuredDistance > MINIMAL_DISTANCE)) {
+                    motorController.forward();
+                    System.out.println("None blocked, move forward");
+
+                } else if ((frontLeftMeasuredDistance < MINIMAL_DISTANCE)
+                        && (frontMiddleMeasuredDistance > MINIMAL_DISTANCE)
+                        && (frontRightMeasuredDistance > MINIMAL_DISTANCE)
+                        && (leftMeasuredDistance > MINIMAL_DISTANCE)
+                        && (rightMeasuredDistance > MINIMAL_DISTANCE)) {
+                    motorController.right(45);
+                    System.out.println("FrontLeft blocked, turn right 45 degrees");
+
+                } else if ((frontLeftMeasuredDistance > MINIMAL_DISTANCE)
+                        && (frontMiddleMeasuredDistance < MINIMAL_DISTANCE)
+                        && (frontRightMeasuredDistance > MINIMAL_DISTANCE)
+                        && (leftMeasuredDistance > MINIMAL_DISTANCE)
+                        && (rightMeasuredDistance > MINIMAL_DISTANCE)) {
+                    motorController.left(45);
+                    System.out.println("FrontMiddle blocked, turn left 45 degrees");
+
+                } else if ((frontLeftMeasuredDistance > MINIMAL_DISTANCE)
+                        && (frontMiddleMeasuredDistance > MINIMAL_DISTANCE)
+                        && (frontRightMeasuredDistance < MINIMAL_DISTANCE)
+                        && (leftMeasuredDistance > MINIMAL_DISTANCE)
+                        && (rightMeasuredDistance > MINIMAL_DISTANCE)) {
+                    motorController.left(45);
+                    System.out.println("FrontRight blocked, turn left 45 degrees");
+
+                } else if ((frontLeftMeasuredDistance > MINIMAL_DISTANCE)
+                        && (frontMiddleMeasuredDistance > MINIMAL_DISTANCE)
+                        && (frontRightMeasuredDistance > MINIMAL_DISTANCE)
+                        && (leftMeasuredDistance < MINIMAL_DISTANCE)
+                        && (rightMeasuredDistance > MINIMAL_DISTANCE)) {
+                    motorController.forward();
+                    System.out.println("Left blocked, move forward");
+
+                } else if ((frontLeftMeasuredDistance > MINIMAL_DISTANCE)
+                        && (frontMiddleMeasuredDistance > MINIMAL_DISTANCE)
+                        && (frontRightMeasuredDistance > MINIMAL_DISTANCE)
+                        && (leftMeasuredDistance > MINIMAL_DISTANCE)
+                        && (rightMeasuredDistance < MINIMAL_DISTANCE)) {
+                    motorController.forward();
+                    System.out.println("Right blocked, move forward");
+
+                } else if ((frontLeftMeasuredDistance < MINIMAL_DISTANCE)
+                        && (frontMiddleMeasuredDistance < MINIMAL_DISTANCE)
+                        && (frontRightMeasuredDistance > MINIMAL_DISTANCE)
+                        && (leftMeasuredDistance > MINIMAL_DISTANCE)
+                        && (rightMeasuredDistance > MINIMAL_DISTANCE)) {
+                    motorController.right(90);
+                    System.out.println("FrontLeft and FrontMiddle blocked, turn right 90 degrees");
+
+                } else if ((frontLeftMeasuredDistance > MINIMAL_DISTANCE)
+                        && (frontMiddleMeasuredDistance < MINIMAL_DISTANCE)
+                        && (frontRightMeasuredDistance < MINIMAL_DISTANCE)
+                        && (leftMeasuredDistance > MINIMAL_DISTANCE)
+                        && (rightMeasuredDistance > MINIMAL_DISTANCE)) {
+                    motorController.left(90);
+                    System.out.println("FrontMiddle and FrontRight blocked, turn left 90 degrees");
+//check in
+                } else if ((frontLeftMeasuredDistance > MINIMAL_DISTANCE)
+                        && (frontMiddleMeasuredDistance > MINIMAL_DISTANCE)
+                        && (frontRightMeasuredDistance < MINIMAL_DISTANCE)
+                        && (leftMeasuredDistance < MINIMAL_DISTANCE)
+                        && (rightMeasuredDistance > MINIMAL_DISTANCE)) {
+                    motorController.right(20);
+                    System.out.println("Left and FrontRight blocked, turn right 20 degrees");
+
+                } else if ((frontLeftMeasuredDistance > MINIMAL_DISTANCE)
+                        && (frontMiddleMeasuredDistance > MINIMAL_DISTANCE)
+                        && (frontRightMeasuredDistance > MINIMAL_DISTANCE)
+                        && (leftMeasuredDistance < MINIMAL_DISTANCE)
+                        && (rightMeasuredDistance < MINIMAL_DISTANCE)) {
+                    motorController.forward();
+                    System.out.println("Left and Right blocked, move forward");
+//check in
+                } else if ((frontLeftMeasuredDistance < MINIMAL_DISTANCE)
+                        && (frontMiddleMeasuredDistance > MINIMAL_DISTANCE)
+                        && (frontRightMeasuredDistance > MINIMAL_DISTANCE)
+                        && (leftMeasuredDistance > MINIMAL_DISTANCE)
+                        && (rightMeasuredDistance < MINIMAL_DISTANCE)) {
+                    motorController.left(20);
+                    System.out.println("FrontLeft and Right blocked, turn left 20 degrees");
+
+                } else if ((frontLeftMeasuredDistance < MINIMAL_DISTANCE)
+                        && (frontMiddleMeasuredDistance > MINIMAL_DISTANCE)
+                        && (frontRightMeasuredDistance < MINIMAL_DISTANCE)
+                        && (leftMeasuredDistance > MINIMAL_DISTANCE)
+                        && (rightMeasuredDistance > MINIMAL_DISTANCE)) {
+                    motorController.left(90);
+                    System.out.println("FrontLeft and FrontRight blocked, turn left 90 degrees");
+
+                } else if ((frontLeftMeasuredDistance < MINIMAL_DISTANCE)
+                        && (frontMiddleMeasuredDistance < MINIMAL_DISTANCE)
+                        && (frontRightMeasuredDistance < MINIMAL_DISTANCE)
+                        && (leftMeasuredDistance < MINIMAL_DISTANCE)
+                        && (rightMeasuredDistance < MINIMAL_DISTANCE)) {
+                    motorController.left(180);
+                    System.out.println("All blocked, turn left 180 degrees");
+
+                } else if ((frontLeftMeasuredDistance < MINIMAL_DISTANCE)
+                        && (frontMiddleMeasuredDistance < MINIMAL_DISTANCE)
+                        && (frontRightMeasuredDistance < MINIMAL_DISTANCE)
+                        && (leftMeasuredDistance > MINIMAL_DISTANCE)
+                        && (rightMeasuredDistance > MINIMAL_DISTANCE)) {
+                    motorController.left(90);
+                    System.out.println("FrontLeft, FrontMiddle, FrontRight blocked, turn left 90 degrees");
+
+                } else if ((frontLeftMeasuredDistance < MINIMAL_DISTANCE)
+                        && (frontMiddleMeasuredDistance < MINIMAL_DISTANCE)
+                        && (frontRightMeasuredDistance > MINIMAL_DISTANCE)
+                        && (leftMeasuredDistance < MINIMAL_DISTANCE)
+                        && (rightMeasuredDistance < MINIMAL_DISTANCE)) {
+                    motorController.left(180);
+                    System.out.println("FrontLeft, FrontMiddle, Left, and Right blocked, turn left 180 degrees");
+
+                } else if ((frontLeftMeasuredDistance < MINIMAL_DISTANCE)
+                        && (frontMiddleMeasuredDistance < MINIMAL_DISTANCE)
+                        && (frontRightMeasuredDistance < MINIMAL_DISTANCE)
+                        && (leftMeasuredDistance > MINIMAL_DISTANCE)
+                        && (rightMeasuredDistance < MINIMAL_DISTANCE)) {
+                    motorController.left(90);
+                    System.out.println("FrontLeft, FrontMiddle, FrontRight, and Right blocked, turn left 90 degrees");
+
+                } else if ((frontLeftMeasuredDistance < MINIMAL_DISTANCE)
+                        && (frontMiddleMeasuredDistance < MINIMAL_DISTANCE)
+                        && (frontRightMeasuredDistance < MINIMAL_DISTANCE)
+                        && (leftMeasuredDistance < MINIMAL_DISTANCE)
+                        && (rightMeasuredDistance > MINIMAL_DISTANCE)) {
+                    motorController.right(90);
+                    System.out.println("FrontLeft, FrontMiddle, FrontRight, and Left blocked, turn right 90 degrees");
+
+                } else if ((frontLeftMeasuredDistance > MINIMAL_DISTANCE)
+                        && (frontMiddleMeasuredDistance < MINIMAL_DISTANCE)
+                        && (frontRightMeasuredDistance < MINIMAL_DISTANCE)
+                        && (leftMeasuredDistance < MINIMAL_DISTANCE)
+                        && (rightMeasuredDistance < MINIMAL_DISTANCE)) {
+                    motorController.left(180);
+                    System.out.println("FrontMiddle, FrontRight, Left, and Right blocked, turn left 180 degrees");
+
+                } else if ((frontLeftMeasuredDistance > MINIMAL_DISTANCE)
+                        && (frontMiddleMeasuredDistance < MINIMAL_DISTANCE)
+                        && (frontRightMeasuredDistance < MINIMAL_DISTANCE)
+                        && (leftMeasuredDistance < MINIMAL_DISTANCE)
+                        && (rightMeasuredDistance > MINIMAL_DISTANCE)) {
+                    motorController.right(90);
+                    System.out.println("FrontMiddle, Front Right, and Left blocked, turn right 90 degrees");
+
+                } else if ((frontLeftMeasuredDistance < MINIMAL_DISTANCE)
+                        && (frontMiddleMeasuredDistance < MINIMAL_DISTANCE)
+                        && (frontRightMeasuredDistance > MINIMAL_DISTANCE)
+                        && (leftMeasuredDistance > MINIMAL_DISTANCE)
+                        && (rightMeasuredDistance < MINIMAL_DISTANCE)) {
+                    motorController.left(90);
+                    System.out.println("FrontLeft, FrontMiddle, and Right blocked, turn left 90 degrees");
+
+                } else if ((frontLeftMeasuredDistance > MINIMAL_DISTANCE)
+                        && (frontMiddleMeasuredDistance > MINIMAL_DISTANCE)
+                        && (frontRightMeasuredDistance < MINIMAL_DISTANCE)
+                        && (leftMeasuredDistance < MINIMAL_DISTANCE)
+                        && (rightMeasuredDistance < MINIMAL_DISTANCE)) {
+                    motorController.left(20);
+                    System.out.println("FrontRight, Left, Right blocked: turn left 20 degrees");
+
+                } else if ((frontLeftMeasuredDistance < MINIMAL_DISTANCE)
+                        && (frontMiddleMeasuredDistance > MINIMAL_DISTANCE)
+                        && (frontRightMeasuredDistance > MINIMAL_DISTANCE)
+                        && (leftMeasuredDistance < MINIMAL_DISTANCE)
+                        && (rightMeasuredDistance < MINIMAL_DISTANCE)) {
+                    motorController.right(20);
+                    System.out.println("FrontLeft, Left, Right blocked: turn right 20 degrees");
+
+                } else if ((frontLeftMeasuredDistance < MINIMAL_DISTANCE)
+                        && (frontMiddleMeasuredDistance > MINIMAL_DISTANCE)
+                        && (frontRightMeasuredDistance < MINIMAL_DISTANCE)
+                        && (leftMeasuredDistance < MINIMAL_DISTANCE)
+                        && (rightMeasuredDistance < MINIMAL_DISTANCE)) {
+                    motorController.left(180);
+                    System.out.println("FrontMiddle, FrontRight, Left, Right blocked: turn left 180 degrees");
+
+                } else if ((frontLeftMeasuredDistance < MINIMAL_DISTANCE)
+                        && (frontMiddleMeasuredDistance > MINIMAL_DISTANCE)
+                        && (frontRightMeasuredDistance < MINIMAL_DISTANCE)
+                        && (leftMeasuredDistance > MINIMAL_DISTANCE)
+                        && (rightMeasuredDistance < MINIMAL_DISTANCE)) {
+                    motorController.left(180);
+                    System.out.println("FrontMiddle, FrontRight, Left, Right blocked: turn left 180 degrees");
+                }
+
+
+                System.out.println("Autonomous: Motor and directions updated -- get to next");
+            }
+
+
+        } catch (Exception ex)
+
+        {
+            System.out.println("Encountered Exception: " + ex);
+        }
+
+        // Stop car
+        motorController.stop();
+
+        // Stop sensors
+        frontright.shutdown();
+        frontmiddle.shutdown();
+        frontleft.shutdown();
+        leftSensor.shutdown();
+        rightSensor.shutdown();
+
+
+        // stop all GPIO activity/threads by shutting down the GPIO controller
+        // (this method will forcefully shutdown all GPIO monitoring threads and scheduled tasks)
+        gpio.shutdown();
+        System.out.println("Exiting ControlGpioExample");
     }
 }
