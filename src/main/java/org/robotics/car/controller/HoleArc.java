5db76367d1232cc2dd66bef5a09b754ef83c39ed
tresgy <morelikemaurice@gmail.com>
2018-04-04 15:49:32 -0700

    Implemented a HoleArc container class that represents a gap between two solid objects as detected by the distance sensor.

---
+++ src/main/java/org/robotics/car/controller/HoleArc.java
@@ -0,0 +1,38 @@
+package org.robotics.car.controller;
+
+public class HoleArc
+{
+    public double   startCritAngle = 0, // Starting critical angle
+                    endCritAngle = 0, // Ending critical angle
+                    startRadius = 0, // Distance at starting angle
+                    endRadius = 0, // Distance at ending angle.
+                    startCritPositionX = 0, startCritPositionY = 0, // The start bound crit point in cartesian space.
+                    endCritPositionX = 0, endCritPositionY = 0, // The end bound crit point in cartesian space
+                    midpointPositionX = 0, midpointPositionY = 0, // The midpoint between the crit points in cartesian space
+                    holeWidth = 0; // The size of the hole / width between crit points.
+
+    public HoleArc(double startCritAngle, double endCritAngle, double startRadius, double endRadius)
+    {
+        // Initial values
+        this.startCritAngle = startCritAngle;
+        this.endCritAngle = endCritAngle;
+        this.startRadius = startRadius;
+        this.endRadius = endRadius;
+
+        // Start / End Position
+        startCritPositionX = startRadius * Math.cos(startCritAngle);
+        startCritPositionY = startRadius * Math.sin(startCritAngle);
+
+        endCritPositionX = endRadius * Math.cos(endCritAngle);
+        endCritPositionY = endRadius * Math.sin(endCritAngle);
+
+        // Midpoint Position
+        midpointPositionX = ( startCritPositionX - endCritPositionX ) / 2;
+        midpointPositionY = (startCritPositionY - endCritPositionY) / 2;
+
+        // Midpoint Size
+        double deltaX = startCritPositionX - endCritPositionX;
+        double deltaY = startCritPositionY - endCritPositionY;
+        holeWidth = Math.sqrt( deltaX*deltaX + deltaY*deltaY );
+    }
+}
