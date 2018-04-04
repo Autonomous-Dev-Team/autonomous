f98eaa24c232020f050ecbc96713af7a96b91d0d
tresgy <morelikemaurice@gmail.com>
2018-04-04 15:54:10 -0700

    Updated HoleArc to have getters. HoleArc is assumed to be an immutable type.

--- src/main/java/org/robotics/car/controller/HoleArc.java
+++ src/main/java/org/robotics/car/controller/HoleArc.java
@@ -2,7 +2,7 @@ package org.robotics.car.controller;
 
 public class HoleArc
 {
-    public double   startCritAngle = 0, // Starting critical angle
+    private double   startCritAngle = 0, // Starting critical angle
                     endCritAngle = 0, // Ending critical angle
                     startRadius = 0, // Distance at starting angle
                     endRadius = 0, // Distance at ending angle.
@@ -34,5 +34,49 @@ public class HoleArc
         double deltaX = startCritPositionX - endCritPositionX;
         double deltaY = startCritPositionY - endCritPositionY;
         holeWidth = Math.sqrt( deltaX*deltaX + deltaY*deltaY );
+    }
+
+    public double getStartCritAngle() {
+        return startCritAngle;
+    }
+
+    public double getEndCritAngle() {
+        return endCritAngle;
+    }
+
+    public double getStartRadius() {
+        return startRadius;
+    }
+
+    public double getEndRadius() {
+        return endRadius;
+    }
+
+    public double getStartCritPositionX() {
+        return startCritPositionX;
+    }
+
+    public double getStartCritPositionY() {
+        return startCritPositionY;
+    }
+
+    public double getEndCritPositionX() {
+        return endCritPositionX;
+    }
+
+    public double getEndCritPositionY() {
+        return endCritPositionY;
+    }
+
+    public double getMidpointPositionX() {
+        return midpointPositionX;
+    }
+
+    public double getMidpointPositionY() {
+        return midpointPositionY;
+    }
+
+    public double getHoleWidth() {
+        return holeWidth;
     }
 }
